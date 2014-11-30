c846ab6c44be1aaf1ecbe1cf0771ee3bb0da8b0c
Jay Wengrow <jaywngrw@gmail.com>
2014-11-29 20:22:51 -0600

    ensuring that blank strings are not added as reviews

--- app/assets/javascripts/stuff_ctrl.js
+++ app/assets/javascripts/stuff_ctrl.js
@@ -8,8 +8,10 @@
     $scope.reviews = ["AngularJS is the best thing since sliced bread!", "I love AngularJS, especially when I'm programming!", "AngularJS is waaaaay better than Chinese Checkers."]
 
     $scope.addReview = function(review) {
-      $scope.reviews.push(review);
-      $scope.newReview = "";
+      if(review) {
+        $scope.reviews.push(review);
+        $scope.newReview = "";
+      }
     };
 
     window.scope = $scope;
